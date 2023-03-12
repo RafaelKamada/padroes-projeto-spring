cfccb235219e853121cd58f086c0444977d534bd
RafaelKamada <rafael-kamada2007@hotmail.com>
2023-03-12 00:10:53 -0300

    Adicionado REST para Produtos

---
+++ src/main/java/one/digitalinnovation/gof/model/Produto.java
@@ -0,0 +1,49 @@
+package one.digitalinnovation.gof.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private Double valor;
+	private Integer quantidade;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+}
---
+++ src/main/java/one/digitalinnovation/gof/model/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProdutoRepository extends CrudRepository<Produto, Long>{
+
+}
